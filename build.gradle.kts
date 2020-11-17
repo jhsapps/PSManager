47f213f32802070e561bbd47f6cc0bff923c95ec
jhsapps <janghs0604@gmail.com>
2020-11-18 06:39:44 +0900

    Change gradle script
    Add plugins
    - kapt version 1.4.10
    - application
    
    Add dependencies
    - tonadofx
    - kohttp
    - moshi (kotlin, codegen)
    
    Add some options
    - mainClassName
    - jvmTarget to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    kotlin("kapt") version "1.4.10"
+    application
 }
 
 group = "com.jhsapps"
@@ -9,6 +11,19 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClassName = "com.jhsapps.MainKt"
+}
+
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:1.7.20")
+    implementation(group = "io.github.rybalkinsd", name = "kohttp", version = "0.12.0")
+    implementation("com.squareup.moshi:moshi:1.11.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
 }
